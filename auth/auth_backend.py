dd07c6fa3e7438a9780cef874f535a2c80bd159d
aditya635 <55912563+aditya635@users.noreply.github.com>
2022-03-10 00:57:55 +0530

    migrated to jwt tokens

--- auth/auth_backend.py
+++ auth/auth_backend.py
@@ -1,4 +1,4 @@
-from fastapi_users.authentication import JWTStrategy,AuthenticationBackend, CookieTransport
+from fastapi_users.authentication import JWTStrategy,AuthenticationBackend, CookieTransport,BearerTransport
 from dotenv import load_dotenv
 load_dotenv()
 import os 
@@ -11,11 +11,11 @@ SECRET = os.environ["SECRET"]
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
 
-cookie_transport = CookieTransport(cookie_max_age=3600)
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
 auth_back = AuthenticationBackend(
     name= "jwt",
-    transport = cookie_transport,
+    transport = bearer_transport,
     get_strategy=get_jwt_strategy,
 )
 
