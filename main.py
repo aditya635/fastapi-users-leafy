daac61b41f0dc43aacdbd84c0c925ec72456c45d
aditya635 <55912563+aditya635@users.noreply.github.com>
2022-04-19 03:45:34 +0530

    CORS

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import imp
 from fastapi import FastAPI,Depends
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_users import FastAPIUsers
 from schemas.user_schemas import UserDB,UserCreate,UserUpdate,User
 from utils.user_manager import get_user_manager
@@ -10,6 +11,21 @@ from ml import model_ini
 from routers import files
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(
     fastapi_users.get_auth_router(auth_back),
     prefix="/auth/jwt",
